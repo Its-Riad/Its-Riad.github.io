0ff26445f24c4ab014068e8f8ba13d3b46a9b1dd
Its-Riad <r.m.ghoneim@lse.ac.uk>
2026-01-06 17:00:37 +0000

    Add files via upload

--- sraper.py
+++ sraper.py
@@ -1,3 +1,8 @@
-import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path='path/to/chromedriver')
+driver = webdriver.Chrome(service=service)
+
 
 
